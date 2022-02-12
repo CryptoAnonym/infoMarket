dd8a644560dc3c23fde290b2ea5fe83df7367094
aLitwin <newlitwin@gmail.com>
2022-02-12 22:21:01 +0100

    validate the input text

--- zonda.py
+++ zonda.py
@@ -1,3 +1,4 @@
+from msilib.schema import Error
 import requests
 
 daneZonda = requests.get("https://api.zonda.exchange/rest/trading/ticker")
@@ -6,20 +7,30 @@ if daneZonda.ok == True:
     dane3 = daneZonda.json()["items"]
 
     def exchange():
-        def zonda(rynek):               # wyszukiwarka rynków w zondzie(bitbay)  
-                
-            test = dane3[rynek]
+        def zonda(rynek): # wyszukiwarka rynków w zondzie(bitbay)  
+                     
+                test = dane3[rynek]
 
-            bid = test["highestBid"]
-            ask = test["lowestAsk"]
-
-            print("\nNajwyższa oferta kupna: " + str(bid)[0:6] + " " + str(rynek)[4:7] )
-            print("Najniższa oferta sprzedaży: " + str(ask)[0:6] + " "+ str(rynek)[4:7] + "\n")
+                bid = test["highestBid"]
+                ask = test["lowestAsk"]
+            
+            
+                print("\nNajwyższa oferta kupna: " + str(bid)[0:6] + " " + str(rynek)[4:7] )
+                print("Najniższa oferta sprzedaży: " + str(ask)[0:6] + " "+ str(rynek)[4:7] + "\n")
+            
 
         print("\n***** Witaj w wyszukiwarce rynków na giełdzie ZONDA! *****")
         print("        (Rynek wyszukujemy wg schematu: BTC-PLN)     ")
-        dane = input("\nPodaj rynk, którego szukasz: ")
+        
+        dane = input("\nPodaj rynek, którego szukasz: ")
 
-        zonda(dane.upper())
+        if len(dane) == 7:
+            if dane.find("-") != -1:
+                zonda(dane.upper())
+            else :
+                print("Błędny rynek!")
+        else :
+            print("Błędny rynek!")
+
 else:
     print("Connect with Zonda API ERROR!")
